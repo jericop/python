7a3d89a3f09051705594bf68135b2e71d3aa0c69
Jerico Pena <jericop@gmail.com>
2025-09-21 13:34:45 -0400

    Test latest changes

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,11 +13,16 @@ source "${ROOT_DIR}/scripts/.util/tools.sh"
 source "${ROOT_DIR}/scripts/.util/print.sh"
 
 function main {
-  local image_ref token
+  local archive_path image_ref token
   token=""
 
   while [[ "${#}" != 0 ]]; do
     case "${1}" in
+    --archive-path | -a)
+      archive_path="${2}"
+      shift 2
+      ;;
+
     --image-ref | -i)
       image_ref="${2}"
       shift 2
@@ -50,11 +55,18 @@ function main {
     util::print::error "--image-ref is required"
   fi
 
+  if [[ -z "${archive_path:-}" ]]; then
+    util::print::info "Using default archive path: ${ROOT_DIR}/build/buildpack-release-artifact.tgz"
+    archive_path="${ROOT_DIR}/build/buildpack-release-artifact.tgz"
+  else
+    archive_path="${archive_path}"
+  fi
+
   repo::prepare
 
   tools::install "${token}"
 
-  buildpack::publish "${image_ref}"
+  buildpack::publish "${image_ref}" "${archive_path}"
 }
 
 function usage() {
@@ -62,6 +74,7 @@ function usage() {
 Publishes a composite buildpack to a registry.
 
 OPTIONS
+  -a, --archive-path <filepath>       Path to the buildpack release artifact (default: ${ROOT_DIR}/build/buildpack-release-artifact.tgz) (optional)
   -h, --help                          Prints the command usage
   -i, --image-ref <ref>               List of image reference to publish to (required)
   -t, --token <token>                 Token used to download assets from GitHub (e.g. jam, pack, etc) (optional)
@@ -84,22 +97,46 @@ function tools::install() {
   util::tools::pack::install \
     --directory "${BIN_DIR}" \
     --token "${token}"
+
+  util::tools::yj::install \
+    --directory "${BIN_DIR}" \
+    --token "${token}"
 }
 
 function buildpack::publish() {
-
-  local image_ref
+  local image_ref archive_path tmp_dir
   image_ref="${1}"
+  archive_path="${2}"
 
   util::print::title "Publishing composite buildpack..."
 
+  util::print::info "Extracting archive..."
+  tmp_dir=$(mktemp -d -p $ROOT_DIR)
+  tar -xvf $archive_path -C $tmp_dir
+
   util::print::info "Publishing buildpack to ${image_ref}"
+
+  cd $tmp_dir
+
+  # If package.toml has no targets we must specify one on the command line, otherwise pack will complain.
+  # This is here for backward compatibility but eventually all package.toml files should have targets defined.
+  targets=""
+  if cat package.toml | yj -tj | jq -r .targets | grep -q null; then
+    # Use the local architecture to support running locally and in CI, which will be linux/amd64 by default.
+    arch=$(util::tools::arch)
+    targets="--target linux/${arch}"
+    echo "package.toml has no targets so ${targets} will be used"
+  fi
 
   pack \
     buildpack package "${image_ref}" \
     --config "${ROOT_DIR}/package.toml" \
     --format image \
-    --publish
+    --publish \
+    ${targets}
+
+  cd $ROOT_DIR
+  rm -rf $tmp_dir
 }
 
 main "${@:-}"
